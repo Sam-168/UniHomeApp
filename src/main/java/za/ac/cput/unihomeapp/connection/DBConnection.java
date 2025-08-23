f8e57dcb7cbbb713725de8c55d81163f1d38f197
iamsam786 <samukelondlela2.0@gmail.com>
2025-08-24 00:09:50 +0200

    added some the db connection

--- src/main/java/za/ac/cput/unihomeapp/connection/DBConnection.java
+++ src/main/java/za/ac/cput/unihomeapp/connection/DBConnection.java
@@ -4,10 +4,25 @@
  */
 package za.ac.cput.unihomeapp.connection;
 
+import java.sql.*;
+
 /**
  *
  * @author samuk
  */
 public class DBConnection {
     
+    private static final String URL = "jdbc:mysql://localhost:3306/unihomeaccommodation";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Nomboniso@55";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
