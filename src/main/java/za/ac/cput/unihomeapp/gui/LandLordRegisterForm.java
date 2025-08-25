92d4d0b82df909bd6f728bace8a44a3cca78bc8f
Ayren-Villet <223120030@mycput.ac.za>
2025-08-25 16:05:34 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/unihomeapp/gui/LandLordRegisterForm.java
@@ -0,0 +1,17 @@
+package RegisterForm.za.ac.cput.prtgui.HomePage.LandLord;
+
+import prt.za.ac.cput.prtgui.*;
+import javax.swing.*;
+import java.awt.*;
+import RegisterForm.za.ac.cput.prtgui.HomePage.Background.CirclePanel;
+
+/**
+ *
+ * @author ayren
+ */
+public class LandLordRegisterForm {
+
+    public static void main(String[] args) {
+        new LandLordForm();
+    }
+}
---
+++ src/main/java/za/ac/cput/unihomeapp/gui/LoginMain.java
@@ -0,0 +1,15 @@
+package RegisterForm.za.ac.cput.prtgui.HomePage.LandLord;
+import RegisterForm.za.ac.cput.prtgui.Login.*;
+import javax.swing.*;
+import java.awt.*;
+import RegisterForm.za.ac.cput.prtgui.HomePage.Background.CirclePanel;
+/**
+ *
+ * @author jadar
+ */
+public class LoginMain {
+
+public static void main (String[]args){
+        new LandLordLogin();
+}
+}
