ac5ee663c04c6827e780063a6e215a9115c6cb01
Ayren-Villet <223120030@mycput.ac.za>
2025-08-28 11:53:43 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/unihomeapp/gui/unihomeapp/gui/LoginMain.java
@@ -0,0 +1,13 @@
+package ac.za.cput.unihomeapp.gui;
+import javax.swing.*;
+import java.awt.*;
+/**
+ *
+ * @author jadar
+ */
+public class LoginMain {
+
+public static void main (String[]args){
+        new LandLordLogin();
+}
+}
