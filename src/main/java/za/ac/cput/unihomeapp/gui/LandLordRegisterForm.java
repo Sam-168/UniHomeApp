bd0cc3252e9b0744e74f6b76d0237f4a370179ab
Ayren-Villet <223120030@mycput.ac.za>
2025-08-28 12:11:25 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/unihomeapp/gui/LandLordRegisterForm.java
@@ -0,0 +1,13 @@
+package ac.za.cput.unihomeapp.gui;
+import javax.swing.*;
+import java.awt.*;
+/**
+ *
+ * @author ayren
+ */
+public class LandLordRegisterForm {
+
+    public static void main(String[] args) {
+        new LandLordForm();
+    }
+}
