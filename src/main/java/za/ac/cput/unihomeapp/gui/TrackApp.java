4d84ef159ed0cfd92ce6e65102fffd875b585590
Jada-Rinquest <222871296@mycput.ac.za>
2025-10-07 08:43:54 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/unihomeapp/gui/TrackApp.java
@@ -0,0 +1,12 @@
+package za.ac.cput.unihomeapp.gui;
+
+/**
+ *
+ * @author ayren
+ */
+public class TrackApp {
+    
+     public static void main(String[] args) {
+        new TrackApplication ();
+    }
+}
